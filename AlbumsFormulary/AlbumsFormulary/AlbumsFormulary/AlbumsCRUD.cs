66a8a83d5e3a3da630319b59e3eab9ef7be3679e
aitoredualdnun <aarque@userzoom.com>
2025-01-05 11:39:24 +0100

    feature: Added front for photos crud controller

--- AlbumsFormulary/AlbumsFormulary/AlbumsFormulary/AlbumsCRUD.cs
+++ AlbumsFormulary/AlbumsFormulary/AlbumsFormulary/AlbumsCRUD.cs
@@ -14,54 +14,82 @@ namespace AlbumsFormulary
 
         private async void button1_Click_1(object sender, EventArgs e)
         {
-            var id = int.Parse(textBoxAlbumId.Text);
-            var userId = int.Parse(textBoxAlbumUserId.Text);
-            var title = textBoxAlbumTitle.Text;
-
-            using (var httpClient = new HttpClient())
+            try
             {
-                await httpClient.PostAsJsonAsync("https://localhost:44374/AlbumsCrude/create_new_albums", new CreateAlbumRequest()
+                var id = int.Parse(textBoxAlbumId.Text);
+                var userId = int.Parse(textBoxAlbumUserId.Text);
+                var title = textBoxAlbumTitle.Text;
+
+                using (var httpClient = new HttpClient())
                 {
-                    Id = id,
-                    UserId = userId,
-                    Title = title,
-                });
+                    await httpClient.PostAsJsonAsync("https://localhost:44374/AlbumsCrude/create_new_albums", new CreateAlbumRequest()
+                    {
+                        Id = id,
+                        UserId = userId,
+                        Title = title,
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error ocurred : {ex.Message}");
             }
         }
 
         private async void button2_Click_1(object sender, EventArgs e)
         {
-            var id = int.Parse(UpdateAlbumIdTextBox.Text);
-            var userId = int.Parse(UpdateUserIdTextBox.Text);
-            var title = UpdateAlbumTitleTextBox.Text;
-
-            using (var httpClient = new HttpClient())
+            try
             {
-                await httpClient.PatchAsJsonAsync("https://localhost:44374/AlbumsCrude/Update_album_by_id", new UpdateAlbumRequest()
+                var id = int.Parse(UpdateAlbumIdTextBox.Text);
+                var userId = int.Parse(UpdateUserIdTextBox.Text);
+                var title = UpdateAlbumTitleTextBox.Text;
+
+                using (var httpClient = new HttpClient())
                 {
-                    Id = id,
-                    UserId = userId,
-                    Title = title,
-                });
+                    await httpClient.PatchAsJsonAsync("https://localhost:44374/AlbumsCrude/Update_album_by_id", new UpdateAlbumRequest()
+                    {
+                        Id = id,
+                        UserId = userId,
+                        Title = title,
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error ocurred : {ex.Message}");
             }
 
         }
 
         private async void button3_Click_1(object sender, EventArgs e)
         {
-            var id = int.Parse(DeleteAlbumTextBox.Text);
-            using (var httpClient = new HttpClient())
+            try
             {
-                await httpClient.DeleteAsync($"https://localhost:44374/AlbumsCrude/update_album_by_id?id={id}");
+                var id = int.Parse(DeleteAlbumTextBox.Text);
+                using (var httpClient = new HttpClient())
+                {
+                    await httpClient.DeleteAsync($"https://localhost:44374/AlbumsCrude/update_album_by_id?id={id}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error ocurred : {ex.Message}");
             }
         }
 
         private async void button4_Click_1(object sender, EventArgs e)
         {
-            var id = AlbumTextBox.Text;
-            var albumsJson = await GetStringAsync($"https://localhost:44374/get_album_by_id?id={id}");
-            // Here I use Newtonsoft.Json to deserialize JSON string to User object
-            var albums = JsonConvert.DeserializeObject<IEnumerable<Album>>(albumsJson);
+            try
+            {
+                var id = AlbumTextBox.Text;
+                var albumsJson = await GetStringAsync($"https://localhost:44374/get_album_by_id?id={id}");
+                // Here I use Newtonsoft.Json to deserialize JSON string to User object
+                var albums = JsonConvert.DeserializeObject<IEnumerable<Album>>(albumsJson);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error ocurred : {ex.Message}");
+            }
         }
 
         private async Task<string> GetStringAsync(string url)
--- AlbumsFormulary/AlbumsFormulary/AlbumsFormulary/PhotosCRUD.cs
+++ AlbumsFormulary/AlbumsFormulary/AlbumsFormulary/PhotosCRUD.cs
@@ -23,61 +23,88 @@ namespace AlbumsFormulary
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            var id = int.Parse(createIdTextBox.Text);
-            var albumId = CreateAlbumIdTextBox.Text;
-            var title = CreatePhotoTitleTextBox.Text;
-            var url = CreatePhotoUrlTextBox.Text;
-            var thumbnailUrl = CreatePhotoTUrlTextBox.Text;
-
-            using (var httpClient = new HttpClient())
+            try
             {
-                await httpClient.PostAsJsonAsync("https://localhost:44374/AlbumsCrude/create_new_photo", new CreatePhotoRequest()
+                var id = int.Parse(createIdTextBox.Text);
+                var albumId = CreateAlbumIdTextBox.Text;
+                var title = CreatePhotoTitleTextBox.Text;
+                var url = CreatePhotoUrlTextBox.Text;
+                var thumbnailUrl = CreatePhotoTUrlTextBox.Text;
+
+                using (var httpClient = new HttpClient())
                 {
-                    Id = id,
-                    AlbumId = int.Parse(albumId),
-                    Title = title,
-                    Url = url,
-                    ThumbnailUrl = thumbnailUrl,
-                }); ;
+                    await httpClient.PostAsJsonAsync("https://localhost:44374/AlbumsCrude/create_new_photo", new CreatePhotoRequest()
+                    {
+                        Id = id,
+                        AlbumId = int.Parse(albumId),
+                        Title = title,
+                        Url = url,
+                        ThumbnailUrl = thumbnailUrl,
+                    }); ;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"error ocurred : {ex.Message} ");
             }
         }
 
         private async void button2_Click(object sender, EventArgs e)
         {
-            var id = int.Parse(UpdatePhotoIdTextBox.Text);
-            var albumId = UpatePhotoAlbumIdTextBox.Text;
-            var title = UpdatePhotoTitleTextBox.Text;
-            var url = UpdatePhotoUrlTextBox.Text;
-            var thumbnailUrl = UpdatePhotoTUrlTextBox.Text;
-
-            using (var httpClient = new HttpClient())
+            try
             {
-                await httpClient.PatchAsJsonAsync("https://localhost:44374/AlbumsCrude/update_by_id", new UpdatePhotoRequest()
+                var id = int.Parse(UpdatePhotoIdTextBox.Text);
+                var albumId = UpatePhotoAlbumIdTextBox.Text;
+                var title = UpdatePhotoTitleTextBox.Text;
+                var url = UpdatePhotoUrlTextBox.Text;
+                var thumbnailUrl = UpdatePhotoTUrlTextBox.Text;
+
+                using (var httpClient = new HttpClient())
                 {
-                    Id = id,
-                    AlbumId = int.Parse(albumId),
-                    Title = title,
-                    Url = url,
-                    ThumbnailUrl = thumbnailUrl,
-                }); ;
+                    await httpClient.PatchAsJsonAsync("https://localhost:44374/AlbumsCrude/update_by_id", new UpdatePhotoRequest()
+                    {
+                        Id = id,
+                        AlbumId = int.Parse(albumId),
+                        Title = title,
+                        Url = url,
+                        ThumbnailUrl = thumbnailUrl,
+                    }); ;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"error ocurred : {ex.Message} ");
             }
         }
 
         private async void button3_Click(object sender, EventArgs e)
         {
-            var id = int.Parse(DeletePhotoIdTextBox.Text);
-            using (var httpClient = new HttpClient())
+            try
             {
-                await httpClient.DeleteAsync($"https://localhost:44374/AlbumsCrude/delete_by_id?id={id}");
+                var id = int.Parse(DeletePhotoIdTextBox.Text);
+                using (var httpClient = new HttpClient())
+                {
+                    await httpClient.DeleteAsync($"https://localhost:44374/AlbumsCrude/delete_by_id?id={id}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"error ocurred : {ex.Message} ");
             }
         }
 
         private async void button4_Click(object sender, EventArgs e)
         {
-            var id = GetPhotoIdTextBox.Text;
-            var albumsJson = await GetStringAsync($"https://localhost:44374/get_by_id?id={id}");
-            // Here I use Newtonsoft.Json to deserialize JSON string to User object
-            var photos = JsonConvert.DeserializeObject<IEnumerable<Album>>(albumsJson);
+            try
+            {
+                var id = GetPhotoIdTextBox.Text;
+                var albumsJson = await GetStringAsync($"https://localhost:44374/get_by_id?id={id}");
+                var photos = JsonConvert.DeserializeObject<IEnumerable<Album>>(albumsJson);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"error ocurred : {ex.Message} ");
+            }
         }
 
         private async Task<string> GetStringAsync(string url)
